174e10e05b05cb8d3f3dfeaa9829230c58016009
Larissa <larissaferri9@gmail.com>
2026-09-25 09:36:46 -0300

    chore: reorganiza estrutura

--- Design Patterns/Padrao-Criacional/Abstract-Factory/Vehicles/Factories/LimeTranspot.ts
+++ Design Patterns/Padrao-Criacional/Abstract-Factory/Vehicles/Factories/LimeTranspot.ts
@@ -1,7 +1,7 @@
 import Drone from "../aerial/Drone";
 import IAircraft from "../aerial/interfaces/IAircraft";
 import ILandVehicle from "../land/interfaces/ILandVehicle";
-import Scooter from "../Land/Scooter";
+import Scooter from "../land/Scooter";
 import ITransportFactory from "./interfaces/ITransportFactory";
 
 export default class LimeTransport implements ITransportFactory{
